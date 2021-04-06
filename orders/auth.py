6e9f86ce300b76e93c52307e3e18c7ad4f3dca05
Victor Arteaga <xathovic@gmail.com>
2021-04-06 01:51:21 -0500

    Add comments

--- orders/auth.py
+++ orders/auth.py
@@ -11,7 +11,9 @@ TRAN_KEY = '024h1IlD'
 
 
 class RedirectionAuth:
+    """ Generate Auth """
     def __init__(self, login=LOGIN, tran_key=TRAN_KEY):
+        """ Constructor """
         self.login = login
         self.tran_key = tran_key
         self.nonce = str(random.random())
@@ -19,13 +21,16 @@ class RedirectionAuth:
 
     # Base64(SHA - 1(nonce + seed + secretkey))
     def generate_tran_key(self):
+        """ Generate trankey with nonce, seed and the tran_key """
         tran_k = str(self.nonce + self.seed + self.tran_key)
         return base64.b64encode(hashlib.sha1(tran_k.encode('utf-8')).digest())
 
     def get_nonce(self):
+        """ Encode nonce """
         return base64.b64encode(self.nonce.encode('utf-8'))
 
     def get_auth(self):
+        """ Get all the auth information """
         auth = {'login': self.login,
                 'tranKey': self.generate_tran_key().decode('utf-8'),
                 'nonce': self.get_nonce().decode('utf-8'),
